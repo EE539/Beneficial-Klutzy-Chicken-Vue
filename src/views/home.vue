59a3a1bdfea7044ad2875d0f4be1de7acee58808
Ecenaz <56602339+EE539@users.noreply.github.com>
2023-07-04 13:51:44 +0300

    Commit from TeleportHQ

---
+++ src/views/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home-container">
+    <span class="home-text">Text</span>
+    <button type="button" class="home-button button">Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  metaInfo: {
+    title: 'Beneficial Klutzy Chicken',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Beneficial Klutzy Chicken',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  flex-direction: column;
+}
+.home-text {
+  font-size: 15px;
+  font-style: normal;
+  font-family: Inter;
+  font-weight: 400;
+}
+.home-button {
+  padding-left: var(--dl-space-space-unit);
+}
+</style>
