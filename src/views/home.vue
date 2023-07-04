507de291c3af13d594053595496090efbf745105
Ecenaz <56602339+EE539@users.noreply.github.com>
2023-07-04 13:53:13 +0300

    Commit from TeleportHQ

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,16 @@
   <div class="home-container">
     <span class="home-text">Text</span>
     <button type="button" class="home-button button">Button</button>
+    <lottie-vue-player
+      :src="`https://presentation-website-assets.teleporthq.io/features/lottie.json`"
+      :autoplay="true"
+      :speed="1"
+      :backgroundColor="transparent"
+      class="home-lottie-node"
+    ></lottie-vue-player>
+    <form class="home-form">
+      <button type="button" class="home-button1 button">Button</button>
+    </form>
   </div>
 </template>
 
@@ -36,5 +46,20 @@ export default {
 }
 .home-button {
   padding-left: var(--dl-space-space-unit);
+}
+.home-lottie-node {
+  width: 300px;
+  height: 300px;
+}
+.home-form {
+  width: 799px;
+  height: 305px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.home-button1 {
+  width: 284px;
+  padding-right: 0px;
 }
 </style>
